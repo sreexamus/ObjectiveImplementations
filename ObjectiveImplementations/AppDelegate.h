29934eea73755bed4cde714ee34f473856fac3f3
sreekanth <sreekanthreddy.iragamreddy@schwab.com>
2019-05-25 13:13:37 -0500

    Initial Commit

---
+++ ObjectiveImplementations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectiveImplementations
+//
+//  Created by Iragam Reddy, Sreekanth Reddy on 5/25/19.
+//  Copyright © 2019 Iragam Reddy, Sreekanth Reddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectiveImplementations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectiveImplementations
+//
+//  Created by Iragam Reddy, Sreekanth Reddy on 5/25/19.
+//  Copyright © 2019 Iragam Reddy, Sreekanth Reddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
